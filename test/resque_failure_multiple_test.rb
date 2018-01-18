73323942010c28f64cdaf35af25504c6dd68a114
Dimitris Bachtis <dimitris.bachtis@skroutz.gr>
2018-01-18 14:56:08 +0200

    Multiple failure backend guards individual backend.save
    Each individual backend.save execution is rescued so that all backends
    get a chance to save each failure.
    After all saves have finished the multiple backend re-raises the last
    seen exception.

--- test/resque_failure_multiple_test.rb
+++ test/resque_failure_multiple_test.rb
@@ -1,5 +1,7 @@
 require 'test_helper'
 require 'resque/failure/multiple'
+require 'resque/failure/redis'
+require 'resque/failure/extra'
 
 describe "Resque::Failure::Multiple" do
 
@@ -12,5 +14,42 @@ describe "Resque::Failure::Multiple" do
       Resque::Failure.create({:exception => exception, :worker => worker, :queue => "queue", :payload => payload})
       Resque::Failure::Multiple.requeue_all # should not raise an error
     end
+  end
+end
+
+module Resque
+  module Failure
+    # A failure backend that only raises errors on save
+    class ExceptionRaiser < Base
+      extend Resque::Failure::Extra
+
+      def save
+        raise StandardError.exception("Bazinga on save")
+      end
+    end
+  end
+end
+
+context "Resque::Failure::Multiple" do
+  setup do
+    exception      = StandardError.exception("This is a test")
+    worker         = Resque::Worker.new(:test)
+    queue          = "queue"
+    payload        = { "class" => Object, "args" => 3 }
+
+    Resque::Failure::Redis.clear #clear redis backends from any previously saved failures
+
+    Resque::Failure::Multiple.configure do |config|
+      config.classes = [Resque::Failure::ExceptionRaiser,Resque::Failure::Redis]
+    end
+
+    @multi = Resque::Failure::Multiple.new(exception, worker, queue, payload)
+  end
+
+  test 'catches exceptions during backend.save and re-raises after all backends have tried to save' do
+    assert_raise StandardError do
+      @multi.save
+    end
+    assert_equal 1, Resque::Failure::Redis.count # Backends registered other than ExceptionRaiser should have saved the failure correctly.
   end
 end
