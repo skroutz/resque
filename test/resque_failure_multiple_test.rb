8a89729c504946790bb2b454c54f5319efd11e04
Agis Anastasopoulos <agis.anast@gmail.com>
2018-05-14 12:44:03 +0300

    Fix test that came with 2f778ed

--- test/resque_failure_multiple_test.rb
+++ test/resque_failure_multiple_test.rb
@@ -3,20 +3,6 @@ require 'resque/failure/multiple'
 require 'resque/failure/redis'
 require 'resque/failure/extra'
 
-describe "Resque::Failure::Multiple" do
-
-  it 'requeue_all and does not raise an exception' do
-    with_failure_backend(Resque::Failure::Multiple) do
-      Resque::Failure::Multiple.classes = [Resque::Failure::Redis]
-      exception = StandardError.exception('some error')
-      worker = Resque::Worker.new(:test)
-      payload = { "class" => Object, "args" => 3 }
-      Resque::Failure.create({:exception => exception, :worker => worker, :queue => "queue", :payload => payload})
-      Resque::Failure::Multiple.requeue_all # should not raise an error
-    end
-  end
-end
-
 module Resque
   module Failure
     # A failure backend that only raises errors on save
@@ -30,26 +16,49 @@ module Resque
   end
 end
 
-context "Resque::Failure::Multiple" do
-  setup do
-    exception      = StandardError.exception("This is a test")
-    worker         = Resque::Worker.new(:test)
-    queue          = "queue"
-    payload        = { "class" => Object, "args" => 3 }
+describe "Resque::Failure::Multiple" do
 
-    Resque::Failure::Redis.clear #clear redis backends from any previously saved failures
-
-    Resque::Failure::Multiple.configure do |config|
-      config.classes = [Resque::Failure::ExceptionRaiser,Resque::Failure::Redis]
+  it 'requeue_all and does not raise an exception' do
+    with_failure_backend(Resque::Failure::Multiple) do
+      Resque::Failure::Multiple.classes = [Resque::Failure::Redis]
+      exception = StandardError.exception('some error')
+      worker = Resque::Worker.new(:test)
+      payload = { "class" => Object, "args" => 3 }
+      Resque::Failure.create({:exception => exception, :worker => worker, :queue => "queue", :payload => payload})
+      Resque::Failure::Multiple.requeue_all # should not raise an error
     end
-
-    @multi = Resque::Failure::Multiple.new(exception, worker, queue, payload)
   end
 
-  test 'catches exceptions during backend.save and re-raises after all backends have tried to save' do
-    assert_raise StandardError do
-      @multi.save
+  describe "backend retry logic" do
+    before do
+      # clear redis backends from any previously saved failures
+      Resque::Failure::Redis.clear
+
+      Resque::Failure::Multiple.configure do |config|
+        config.classes = [Resque::Failure::ExceptionRaiser, Resque::Failure::Redis]
+      end
     end
-    assert_equal 1, Resque::Failure::Redis.count # Backends registered other than ExceptionRaiser should have saved the failure correctly.
+
+    after do
+      Resque::Failure::Multiple.configure do |config|
+        config.classes = [Resque::Failure::Redis]
+      end
+    end
+
+    it 'catches exceptions during backend.save and re-raises after all backends have tried to save' do
+      exception = StandardError.exception("This is a test")
+      worker = Resque::Worker.new(:test)
+      queue = "queue"
+      payload = { "class" => Object, "args" => 3 }
+
+      @multi = Resque::Failure::Multiple.new(exception, worker, queue, payload)
+      assert_raises StandardError do
+        @multi.save
+      end
+
+      # Backends registered other than ExceptionRaiser should have saved the
+      # failure correctly.
+      assert_equal 1, Resque::Failure::Redis.count
+    end
   end
 end
